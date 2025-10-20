4c08d161bd516c638e80f22b8513f39606671a82
ICookieCycloneI <71351781+ICookieCycloneI@users.noreply.github.com>
2025-10-20 22:19:56 +0200

    Changed the entry point and added manual dependancy injection.

--- MediaRatingProject/Program.cs
+++ MediaRatingProject/Program.cs
@@ -1,19 +1,28 @@
 ﻿namespace MediaRatingProject
 {
-    using MediaRatingProject.Server;
     using MediaRatingProject.API;
+    using MediaRatingProject.API.Services;
+    using MediaRatingProject.API.Controllers;
+    using MediaRatingProject.Data.Stores;
 
     internal class Program
     {
         static void Main(string[] args)
         {
             string[] prefix = { "http://localhost:8080/" };
-            APIListener api = new(prefix);
-            // api.Start();
+            
+            // Temporary secret for JWT generation:
+            string secret = "temporary_secret_key_that_should_be_hidden_and_not_shown_publicly_like_this";
+            var jwtService = new JwtService(secret);
 
-            EndPointsListner listener = new(prefix, new RequestParser(new UsersHandler()));
+            // Manual set up of the project with dependancy injection.
+            var userController = new UsersController(new UserStore(), jwtService);
+            var mediaController = new MediaController(new MediaStore());            
+            var requestParser = new RequestParser(userController);
+            var requestHandler = new RequestHandler(userController, mediaController);  
+
+            APIListener listener = new(prefix, requestParser, requestHandler);
             listener.Start();
-
         }
     }
 }
