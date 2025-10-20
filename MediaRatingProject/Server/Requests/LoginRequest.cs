c44cd05b64f754100f3a09b7c20a6fa3cd7a642b
ICookieCycloneI <71351781+ICookieCycloneI@users.noreply.github.com>
2025-10-20 22:40:46 +0200

    Removed redundant classes

--- MediaRatingProject/Server/Requests/LoginRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿// FOR DEMONSTRATION ONLY, ABSOLUTELY UNSAFE.
-namespace MediaRatingProject.Server.Requests
-{
-    public class LoginRequest
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
