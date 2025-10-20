710a265c39d8844bbf88e2f43fb2bed242e1a308
ICookieCycloneI <71351781+ICookieCycloneI@users.noreply.github.com>
2025-10-20 02:13:59 +0200

    Restructured project

---
+++ MediaRatingProject.API/Interfaces/IRequestVisitor.cs
@@ -0,0 +1,12 @@
+﻿using MediaRatingProject.API.Requests;
+
+namespace MediaRatingProject.API.Interfaces
+{
+    public interface IRequestVisitor
+    {
+        void Visit(LoginPOSTRequest request);
+        void Visit(RegisterPOSTRequest request);
+        void Visit(FailedRequest request);
+
+    }
+}
